c2b671b9b978adf5bcc54a8c584bb2d8905a970a
D1zichek <parkydarky353@gmail.com>
2025-05-22 15:14:20 +0300

    u3

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include "game.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
+    
+    if (argc > 1 && std::string(argv[1]) == "--test") {
+        std::cout << "Everything works!" << std::endl;
+        return 0;
+    }
+    
     int choice;
     loadParameters();
 
